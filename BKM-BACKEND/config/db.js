ce451d0a813f7baccb432c256c758db253ee64d3
Imrane_Yt <imrane.uchiha.10@gmail.com>
2025-06-04 21:58:09 +0100

    cleaning code v1

--- BKM-BACKEND/config/db.js
+++ BKM-BACKEND/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 // Configuration de la connexion avec gestion explicite de l'encodage UTF-8
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3307,
+  port: 3306,
   user: 'root',
   password: '',
   database: 'locationvoitures',
