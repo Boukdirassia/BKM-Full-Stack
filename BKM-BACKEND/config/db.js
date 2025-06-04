a68fcef9b3c5297fdad4352fd31a1d132596da62
Assia Boukdir <Boukdirassia@gmail.com>
2025-06-04 16:35:13 +0000

    Historique

--- BKM-BACKEND/config/db.js
+++ BKM-BACKEND/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 // Configuration de la connexion avec gestion explicite de l'encodage UTF-8
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3306,
+  port: 3307,
   user: 'root',
   password: '',
   database: 'locationvoitures',
